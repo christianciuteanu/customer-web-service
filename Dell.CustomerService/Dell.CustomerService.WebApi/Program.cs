f767d5a3d5c95f8f79e991514f1ce3e893d4051e
ChristianCiuteanu <christianciuteanu@gmail.com>
2018-10-27 23:18:30 +0300

    Host App in a Windows service

--- Dell.CustomerService/Dell.CustomerService.WebApi/Program.cs
+++ Dell.CustomerService/Dell.CustomerService.WebApi/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
 
 namespace Dell.CustomerService.Web.Api
@@ -10,8 +13,46 @@ namespace Dell.CustomerService.Web.Api
 		public static void Main(string[] args)
 		{
 			BuildWebHost(args).Run();
+
+			//RunServiceAsAService(args);
 		}
 
+		/// <summary>
+		/// Use this method to Host ASP.NET Core in a Windows Service
+		/// </summary>
+		/// <param name="args"></param>
+		public static void RunServiceAsAService(string[] args)
+		{
+			var isService = !(Debugger.IsAttached || args.Contains("--console"));
+			var pathToContentRoot = Directory.GetCurrentDirectory();
+			var webHostArgs = args.Where(arg => arg != "--console").ToArray();
+
+			if (isService)
+			{
+				var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+				pathToContentRoot = Path.GetDirectoryName(pathToExe);
+			}
+
+			var host = WebHost.CreateDefaultBuilder(webHostArgs)
+				.UseContentRoot(pathToContentRoot)
+				.UseStartup<Startup>()
+				.Build();
+
+			if (isService)
+			{
+				host.RunAsService();
+			}
+			else
+			{
+				host.Run();
+			}
+		}
+
+		/// <summary>
+		/// Use this method to run the app re regularly
+		/// </summary>
+		/// <param name="args"></param>
+		/// <returns></returns>
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.UseKestrel()
@@ -20,6 +61,5 @@ namespace Dell.CustomerService.Web.Api
 				.UseIISIntegration()
 				.UseStartup<Startup>()
 				.Build();
-
 	}
 }
