1666e6fb515a5dbc1c985623e97a33a79aa7cdde
ChristianCiuteanu <christianciuteanu@gmail.com>
2018-10-27 14:43:35 +0300

    Add postgress script to git and update appsettings.json with heroku postgress credentials

---
+++ Dell.CustomerService/DatabaseCreationScript.sql
@@ -0,0 +1,29 @@
+﻿---Create dabases---
+CREATE DATABASE "Dell.Customers"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+---Enable extension for auto generating uuid at insert---
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+---Create Customer table---
+CREATE TABLE public."Customer"
+(
+    "Id" uuid NOT NULL DEFAULT uuid_generate_v1(),
+    "Name" text COLLATE pg_catalog."default" NOT NULL,
+    "Email" text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customer_pkey PRIMARY KEY ("Id")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."Customer"
+    OWNER to postgres;
